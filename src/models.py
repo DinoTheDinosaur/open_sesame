46ff5f70354a96df025727b8d4093d6a67eca9e7
Alexander Zorkin <somnoynadno@yandex.ru>
2019-02-01 15:43:08 +0700

    Update profiles, add functions to work with pickle

--- src/models.py
+++ src/models.py
@@ -1,3 +1,5 @@
+import pickle
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -6,8 +8,6 @@ import scipy.io.wavfile as wav
 
 from sklearn.mixture import GaussianMixture
 from sklearn import preprocessing
-
-import pickle
 
 # all classes take mfcc of audio as argument in fit()
 # all classes take mfcc as argument in predict()
@@ -24,7 +24,7 @@ def mfcc2features(mfcc):
         features.append(feature)
     return features
 
-# take list of features and return mean of each
+# take list of features and return mean of each in a list
 def average_of_features(features):
 	mean_features = list()
 	for i in range(features.shape[0]):
@@ -32,23 +32,12 @@ def average_of_features(features):
 
 	return mean_features
 
-# take 2 lists of mean_features, return error
+# take 2 lists of mean_features, return error as float
 def count_error_square(nonorig, orig):
 	error = 0
 	for i in zip(nonorig, orig):
 		error += abs(i[1]**2 - i[0]**2)
 	return error
-
-# uses to count probability
-def count_min_error(features, mean):
-	min_er = 10**8
-	if features.shape[0] == 1:
-		raise Exception("Only one audio")
-	for feature in features:
-		er = count_error_square(feature, mean)
-		if er < min_er:
-			min_er = er
-	return min_er
 
 # taking mfcc from wav
 def take_mfcc(path):
@@ -66,19 +55,20 @@ class GMM_Voice_Profile:
 		self.__gmm.fit(mfcc_of_voice)
 		self.__default_error = self.__gmm.score(mfcc_of_voice)
 
+	def _score(self, mfcc_of_voice):
+		assert __gmm
+		return self.__gmm.score(mfcc_of_voice)
+
 	def predict(self, mfcc_of_voice):
 		assert self.__default_error != 0
 		mfcc_of_voice = preprocessing.scale(mfcc_of_voice)
 		current_proba = self.__default_error/self.__gmm.score(mfcc_of_voice)
 
-		if current_proba > 1:
-			current_proba = 1
-
-		with open("Voice_Profiles.pkl", 'rb') as f:
+		with open("Voice_Profiles.pickle", 'rb') as f:
 			voice_profiles = pickle.load(f)
 			for user in voice_profiles["GMM"]:
 				user_GMM = voice_profiles["GMM"].get(user)
-				proba = self.__default_error/user_GMM.score(mfcc__of_voice)
+				proba = self.__default_error/user_GMM._score(mfcc__of_voice)
 				if proba > current_proba:
 					print("No")
 					break
@@ -88,7 +78,7 @@ class GMM_Voice_Profile:
 
 class Voice_Profile:
 	__mean_features = list()
-	__default_error = 0
+
 	def fit(self, mfcc_of_voice):
 		features = list()
 		mfcc_feat = mfcc2features(mfcc_of_voice)
@@ -97,22 +87,69 @@ class Voice_Profile:
 		features = features.transpose()
 
 		self.__mean_features = average_of_features(features)
-	
-		self.__default_error = count_min_error(features.transpose(), self.__mean_features)
 
-	def predict(self, mfcc_of_voice):
-		assert self.__default_error != 0
-		
+	def _score(self, mfcc_of_voice):
+		assert len(self.__mean_features)
+
 		mfcc_feat = mfcc2features(mfcc_of_voice)
+		return count_error_square(mfcc_feat, self.__mean_features)
 		
-		error = count_error_square(mfcc_feat, self.__mean_features)
-		proba = self.__default_error/error
-		if proba > 1:
-			proba = 1
-		print(proba)
+	def predict(self, mfcc_of_voice):
+		assert len(self.__mean_features)
+		
+		current_error = self._score(mfcc_of_voice)
 
+		with open("Voice_Profiles.pickle", 'rb') as f:
+			voice_profiles = pickle.load(f)
+			for user in voice_profiles["Simple"]:
+				user_Simple = voice_profiles["Simple"].get(user)
+				error = user_Simple._score(mfcc__of_voice)
+				if error < current_error:
+					print("No")
+					break
+
+		print("Yes")
+		
 class CNN_Voice_Profile:
 	def fit(self, voices):
 		pass
 	def predict(self, voice):
 		pass
+
+
+def create_empty_pickle():
+	data = {"Simple": {}, "GMM": {}, "CNN": {}}
+	with open('Voice_Profiles.pickle', 'wb') as f:
+		pickle.dump(data, f)
+
+# take nickname and mfcc of voice, create profiles in pickle
+def add_user(nickname, mfcc_of_voice):
+	with open('Voice_Profiles.pickle', 'rb') as f:
+		profiles = pickle.load(f)
+
+		profiles.get("Simple")[nickname] = Voice_Profile()
+		profiles.get("Simple")[nickname].fit(mfcc_of_voice)
+
+		profiles.get("GMM")[nickname] = GMM_Voice_Profile()
+		profiles.get("GMM")[nickname].fit(mfcc_of_voice)
+		
+		# profiles.get("CNN")[nickname] = GMM_Voice_Profile()
+		# profiles.get("CNN")[nickname].fit(mfcc_of_voice)
+
+	with open('Voice_Profiles.pickle', 'wb') as f:
+		pickle.dump(profiles, f)
+
+# return true if user doesn't exist in pickle
+def check_user(nickname):
+	with open('Voice_Profiles.pickle', 'rb') as f:
+		profiles = pickle.load(f)
+		if nickname not in profiles.get("Simple") and nickname not in profiles.get("GMM") and nickname not in profiles.get("CNN"):
+			return True
+		return False
+
+def print_user_list():
+	with open('Voice_Profiles.pickle', 'rb') as f:
+		profiles = pickle.load(f)
+		print(profiles.get("GMM").keys())
+
+
